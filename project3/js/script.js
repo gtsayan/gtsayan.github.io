e8ef56427214b184c7c90ccb2f29dc4ecfb38714
gtsayan <48355689+gtsayan@users.noreply.github.com>
2020-01-21 14:34:58 +0300

    Add files via upload

---
+++ project3/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    console.log("test")
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        items: 1,
+        center: true,
+    })
+});
